5f59621c6de8e67d708102035d8381d2f9d6b6c4
sylviot <sylvio.tavares@hotmail.com>
2019-11-15 18:10:18 -0500

    Adicionado port

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ def admin():
   return render_template('backend/index.html')
   
 if __name__ == '__main__':
-  app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host = '0.0.0.0', port = port)
