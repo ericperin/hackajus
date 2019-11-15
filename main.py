1a49298cec168d096e0766512797aae0c03991a4
sylviot <sylvio.tavares@hotmail.com>
2019-11-15 17:58:21 -0500

    Suporte a frontend e backend areas

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask, render_template
 
-template_dir = os.path.abspath('./src/frontend')
+template_dir = os.path.abspath('./src')
 print(template_dir)
 
 app = Flask(__name__, template_folder = template_dir)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('frontend/index.html')
+
+@app.route('/admin')
+def admin():
+  return render_template('backend/index.html')
   
 if __name__ == '__main__':
   app.run(debug=True)
