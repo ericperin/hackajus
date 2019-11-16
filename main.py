47bdfe3fee2dacedaa8b421272cec1ddea2e75e9
sylviot <sylvio.tavares@hotmail.com>
2019-11-16 10:22:30 -0500

    funções para processos

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 from flask import Flask, render_template
+from flask_cors import CORS
 
 template_dir = os.path.abspath('./src')
 print(template_dir)
 
 app = Flask(__name__, template_folder = template_dir, static_url_path = '', static_folder = './src/frontend/dist')
+CORS(app)
+
+data = 0
 
 @app.route('/')
 def index():
@@ -12,12 +16,24 @@ def index():
 
 @app.route('/admin')
 def admin():
-  return render_template('backend/index.html')
+  return render_template('backend/index.html', data = data)
 
 @app.route('/processo')
-def main():
+def processo():
   return render_template('backend/processo.html')
-  
+
+@app.route('/processo/new', methods=['GET', 'POST'])
+def new():
+  global data
+  data = data + 1
+  return ''
+
+@app.route('/processo/reset', methods=["GET"])
+def reset():
+  global data
+  data = 0
+  return ''
+
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
   app.run(host = '0.0.0.0', port = port, debug = True)
