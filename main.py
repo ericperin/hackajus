5d09faa6f191b2a27309cf64b7e383a25096b962
sylviot <sylvio.tavares@hotmail.com>
2019-11-15 19:09:32 -0500

    static folder para acesso a dist assets

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from flask import Flask, render_template
 template_dir = os.path.abspath('./src')
 print(template_dir)
 
-app = Flask(__name__, template_folder = template_dir)
+app = Flask(__name__, template_folder = template_dir, static_url_path = '', static_folder = './src/frontend/dist')
 
 @app.route('/')
 def index():
-  return render_template('frontend/index.html')
+  return render_template('frontend/dist/index.html')
 
 @app.route('/admin')
 def admin():
@@ -16,4 +16,4 @@ def admin():
   
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
-  app.run(host = '0.0.0.0', port = port)
+  app.run(host = '0.0.0.0', port = port, debug = True)
